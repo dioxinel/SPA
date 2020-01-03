e1e60e9e040060a4812f98b7470648284b2a831d
Andriy <dioxine2000@gmail.com>
2020-01-03 21:52:42 +0200

    Update discription of TV Shows

--- main.js
+++ main.js
@@ -4,83 +4,6 @@ let baseImageURL = "https://image.tmdb.org/t/p/";
 let posterSize = 'w185';
 
 
-let hidOrDisplay = function () {
-  let obj = this.parentNode.lastChild;
-  if(obj.style.display == "none") {
-     obj.style.display = "";
-   }
-   else {
-     obj.style.display = "none";
-  }
-}
-
-let createTitle = function (show, data, outputObj) {
-  let Name = data.results[show].name;
-  let TV_Show = document.createElement('li');
-  let TV = document.createElement('a');
-  TV.innerHTML = Name;
-  TV.addEventListener('click', hidOrDisplay);
-  TV_Show.append(TV);
-  TV_Show.append(createDiscription(Name, show, data));
-  outputObj.append(TV_Show);
-}
-
-let createDiscription = function (Name, show, data) {
-  let discription = document.createElement('div');
-  discription.id = Name;
-  discription.style.display = 'none';
-  discription.append(displayPoster(show, data, Name));
-  discription.append(displayOverview(show, data));
-  let tvId = data.results[show].id;
-  let url = "".concat(baseURL, 'tv/', tvId, 'popular?api_key=', APIKEY, '&language=en-US');
-  fetch(url)
-    .then(result=>result.json())
-    .then((data)=>{
-      discription.append(displayNumberOfSeasons(data));
-    })
-  return discription;
-}
-
-let displayOverview = function (show, data) {
-  let overviewShow = document.createElement('p');
-  let overview = data.results[show].overview;
-  overviewShow.innerHTML = overview;
-  return overviewShow;
-  }
-
-let displayPoster = function (show, data, name) {
-  let posterShow = document.createElement('img');
-  posterShow.alt = 'Постер відсутній';
-  let posterPath = data.results[show].poster_path;
-  if (posterPath == null) {
-    return;
-  }
-  posterShow.src = "".concat(baseImageURL, posterSize, posterPath);
-  return posterShow;
-  }
-
-let displayNumberOfSeasons = function (data) {
-  let numberOfSeasons = document.createElement('p');
-  let len = data.seasons[0].episode_count;
-  numberOfSeasons.innerHTML = len;
-  return numberOfSeasons;
-
-}
-  // To get image config
-  // let getConfig = function () {
-  //   let url = "".concat(baseURL, 'configuration?api_key=', APIKEY);
-  //   fetch(url)
-  //     .then((result)=>{
-  //       return result.json();
-  //       })
-  //     .then((data)=>{
-  //       baseImageURL = data.images.secure_base_url;
-  //       configData = data.images;
-  //       console.log(configData);
-  //       })
-  //   }
-
-
 let getPopular = function (num) {
   if (typeof(num) == 'object'){
     num = 1; 
@@ -93,7 +16,6 @@ let getPopular = function (num) {
     let smth = document.createElement('div');
     smth.id = 'output';
     page.append(smth);
-    console.log(data);
     for (let show in data.results) {
       createTitle(show, data, output);
     }
@@ -101,6 +23,7 @@ let getPopular = function (num) {
     updatePagin(lastp, num);
   })
 }
+
 
 let getRated = function (num) {
   if (!num){
@@ -122,8 +45,129 @@ let getRated = function (num) {
 
   })
 }
-// document.addEventListener('DOMContentLoaded', getConfig);
-document.addEventListener('DOMContentLoaded', getPopular);
+
+
+let createTitle = function (show, data, outputObj) {
+  let Name = data.results[show].name;
+  let TV_Show = document.createElement('li');
+  let TV = document.createElement('a');
+  TV.innerHTML = Name;
+  TV.addEventListener('click', hidOrDisplay);
+  TV_Show.append(TV);
+  TV_Show.append(createDiscription(Name, show, data));
+  outputObj.append(TV_Show);
+}
+
+
+let createDiscription = function (Name, show, data) {
+  let discription = document.createElement('div');
+  discription.id = Name;
+  discription.style.display = 'none';
+  discription.append(displayPoster(show, data, Name));
+  discription.append(displayOverview(show, data));
+  let tvId = data.results[show].id;
+  let url = "".concat(baseURL, 'tv/', tvId, 'popular?api_key=', APIKEY, '&language=en-US');
+  fetch(url)
+    .then(result=>result.json())
+    .then((data)=>{
+      discription.append(displayNumberOfSeasons(data));
+      discription.append(displayNumberOfEpisodes(data));
+      discription.append(displayListOfSeasons(data));
+    })
+  return discription;
+}
+
+
+let displayOverview = function (show, data) {
+  let overviewShow = document.createElement('p');
+  let overview = data.results[show].overview;
+  overviewShow.innerHTML = overview;
+  return overviewShow;
+  }
+
+
+let displayPoster = function (show, data, name) {
+  let posterShow = document.createElement('img');
+  posterShow.alt = 'Постер відсутній';
+  let posterPath = data.results[show].poster_path;
+  if (posterPath == null) {
+    return;
+  }
+  posterShow.src = "".concat(baseImageURL, posterSize, posterPath);
+  return posterShow;
+  }
+
+
+let displayNumberOfSeasons = function (data) {
+  let numberOfSeasons = document.createElement('p');
+  let len = data.number_of_seasons;
+  numberOfSeasons.innerHTML = 'Number of seasons: ' + len;
+  return numberOfSeasons;
+}
+
+
+let displayNumberOfEpisodes = function (data) {
+  let numberOfEpisodes = document.createElement('p');
+  let len = data.number_of_episodes;
+  numberOfEpisodes.innerHTML = 'Number of episodes: ' + len;
+  return numberOfEpisodes;
+}
+ 
+
+let displayListOfSeasons = function (data) {
+  let listOfSeasons = document.createElement('div');
+  let len = data.number_of_seasons;
+  for (let i = 0; i < len; i++) {
+    let nameOfSeason = data.seasons[i].name;
+    let season = document.createElement('li');
+    let seasonName = document.createElement('a');
+    seasonName.innerHTML = nameOfSeason;
+    season.append(seasonName);
+    seasonName.append(discriptionOfSeason(data, i));
+    listOfSeasons.append(season);
+    seasonName.addEventListener('click', hidOrDisplayV2);
+  } 
+  return listOfSeasons;
+}
+
+
+let discriptionOfSeason = function(data, numOfSeason) {
+  let discription = document.createElement('div');
+  discription.append(seasonPoster(data, numOfSeason));
+  discription.append(seasonOveriew(data, numOfSeason));
+  discription.append(numOfEpisodesInSeason(data, numOfSeason));
+  discription.style.display = 'none';
+  return discription;
+  }
+
+
+let seasonPoster = function(data, numOfSeason) {
+  let posterOfSeason = document.createElement('img');
+  posterOfSeason.alt = 'Постер відсутній';
+  let posterPath = data.seasons[numOfSeason].poster_path;
+  if (posterPath == null) {
+    return;
+  }
+  posterOfSeason.src = "".concat(baseImageURL, posterSize, posterPath);
+  return posterOfSeason;
+  }
+
+
+ let seasonOveriew = function(data, numOfSeason){
+  let overviewOfSeason = document.createElement('p');
+  let overview = data.seasons[numOfSeason].overview;
+  overviewOfSeason.innerHTML = overview;
+  return overviewOfSeason;
+ }
+
+
+ let numOfEpisodesInSeason = function(data, numOfSeason) {
+  let numberOfEpisodes = document.createElement('p');
+  let len = data.seasons[numOfSeason].episode_count;
+  numberOfEpisodes.innerHTML = 'Number of episodes: ' + len;
+  return numberOfEpisodes;
+ }
+
 
 let createPaginNode = function (num, place) {
   let p = document.createElement('a');
@@ -146,7 +190,7 @@ let router = function  (pageNum) {
   }
 }
 
-window.addEventListener('hashchange', router);
+
 let pagin = function () {
   let center = this.num;
   router(center);
@@ -193,3 +237,44 @@ let updatePagin = function (lastp, center) {
   }
 }
 
+
+let hidOrDisplay = function () {
+  let obj = this.parentNode.lastChild;
+  if(obj.style.display == "none") {
+     obj.style.display = "";
+   }
+   else {
+     obj.style.display = "none";
+  }
+}
+
+
+let hidOrDisplayV2 = function () {
+  let obj = this.lastChild;
+  if(obj.style.display == "none") {
+     obj.style.display = "";
+   }
+   else {
+     obj.style.display = "none";
+  }
+}
+
+
+// document.addEventListener('DOMContentLoaded', getConfig);
+document.addEventListener('DOMContentLoaded', getPopular);
+window.addEventListener('hashchange', router);
+
+
+ // To get image config
+  // let getConfig = function () {
+  //   let url = "".concat(baseURL, 'configuration?api_key=', APIKEY);
+  //   fetch(url)
+  //     .then((result)=>{
+  //       return result.json();
+  //       })
+  //     .then((data)=>{
+  //       baseImageURL = data.images.secure_base_url;
+  //       configData = data.images;
+  //       console.log(configData);
+  //       })
+  //   }
